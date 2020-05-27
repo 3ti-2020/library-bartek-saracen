a798cabe5e0e6c73d71c0c6cb40d1e7e8170d6f2
saroNERO <bartek.s.official@gmail.com>
2020-05-27 19:18:16 +0200

    simple look

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo("start");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteka</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="top"></div>
+        <main></main>
+        <footer></footer>
+    </div>
+</body>
+</html>
